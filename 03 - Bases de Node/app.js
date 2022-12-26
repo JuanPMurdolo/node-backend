692bf9c29a8691ff89b40dbb9912f412b0668533
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-12-26 13:57:17 -0300

    Clase 35

--- 03 - Bases de Node/app.js
+++ 03 - Bases de Node/app.js
@@ -1,5 +1,27 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require('yargs').option('b', {
+    alias : 'base',
+    type : 'number',
+    demandOption : true
+}).check((argv, options) => {
+    if (isNaN (argv.b)){
+        throw 'la base tiene que ser un numero'
+    }
+    return true;
+}).argv;
+const argv2 = require('yargs').option('l', {
+    alias: 'limite',
+    type: 'boolean',
+    demandOption : false,
+    default: true
+}).argv;
 
 console.clear();
-const base = 3;
-crearArchivo(base).then(nombreArchivo => console.log(nombreArchivo, "creado")).catch (err => console.log(err));
+//const base = 3;
+
+//const [,,arg3 = 'base=5'] = process.argv;
+//const [, base] = arg3.split('=');
+console.log(process.argv);
+
+
+crearArchivo(argv.b, argv2.l).then(nombreArchivo => console.log(nombreArchivo, "creado")).catch (err => console.log(err));
--- 03 - Bases de Node/helpers/multiplicar.js
+++ 03 - Bases de Node/helpers/multiplicar.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 
-const crearArchivo = async(base = 5) => {
+const crearArchivo = async(base = 5, limite, multi = 11) => {
 console.log("============================")
 console.log(`        Tabla del ${base}   `)
 console.log("============================")    
 
 let salida = '';
 
-for (let index = 0; index < 11; index++) {
+for (let index = 0; index < multi; index++) {
     if (index < 10){
     salida += `${base} x ${index} = ${base * index}\n`;
     } else {
         salida += `${base} x ${index} = ${base * index}`;
     }
 }
+if (limite === true){
 console.log(salida);
+}
 
 fs.writeFileSync(`tabla-${base}.txt`, salida);
 return `tabla-${base}.txt`
